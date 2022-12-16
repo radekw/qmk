24f271c590f5ea7e5156c07627dd8f091bab302e
Radek Wierzbicki <radekw@me.com>
2022-12-15 23:33:41 -0500

    v3

--- keyboards/preonic/keymaps/radekw/keymap.c
+++ keyboards/preonic/keymaps/radekw/keymap.c
@@ -35,27 +35,27 @@ enum preonic_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_preonic_grid(
-  QK_GESC, KC_1,    KC_2,    KC_3,           KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,     KC_0,     KC_MINS,
-  KC_TAB,  KC_Q,    KC_W,    KC_E,           KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,     KC_BSLS,
-  KC_BSPC, KC_A,    KC_S,    KC_D,           KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_QUOT,
-  KC_LSFT, KC_Z,    KC_X,    KC_C  ,         KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,  KC_RSFT,
-  KC_LCTL, KC_LGUI, KC_LALT, LT(4, QK_LEAD), LOWER,   KC_ENT,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN,  KC_UP,    RCTL_T(KC_RGHT)
+  QK_GESC, KC_1,    KC_2,    KC_3,  KC_4,  KC_5,   KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+  KC_TAB,  KC_Q,    KC_W,    KC_E,  KC_R,  KC_T,   KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+  KC_LCTL, KC_A,    KC_S,    KC_D,  KC_F,  KC_G,   KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LSFT, KC_Z,    KC_X,    KC_C,  KC_V,  KC_B,   KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+  MO(4),   KC_EQL,  KC_LALT, LOWER, MO(4), KC_ENT, KC_SPC, KC_BSPC, RAISE,   KC_LGUI, KC_MINS, KC_RCTL
 ),
 
 [_LOWER] = LAYOUT_preonic_grid(
-  KC_TILD, MEH(KC_Q), MEH(KC_W), MEH(KC_E), MEH(KC_R), MEH(KC_T), MEH(KC_Y), MEH(KC_U), MEH(KC_I), MEH(KC_O), MEH(KC_P), KC_BSPC,
-  KC_NO,   KC_NO,     KC_PSCR,   KC_HOME,   KC_PGUP,   KC_NO,     KC_PSLS,   KC_P7,     KC_P8,     KC_P9,     KC_PMNS,   KC_NO,
-  KC_DEL,  KC_NO,     KC_NO,     KC_END,    KC_PGDN,   KC_NO,     KC_PAST,   KC_P4,     KC_P5,     KC_P6,     KC_PPLS,   KC_NO,
-  KC_NO,   KC_WH_L,   KC_WH_D,   KC_WH_U,   KC_WH_R,   KC_NO,     KC_P0,     KC_P1,     KC_P2,     KC_P3,     KC_EQL,    KC_NO,
-  KC_NO,   KC_NO,     KC_NO,     KC_NO,     LOWER,     KC_UNDS,   KC_SPC,    RAISE,     KC_P0,     KC_PDOT,   KC_NO,     LCA(KC_DEL)
+  KC_TILD, MEH(KC_Q), MEH(KC_W), MEH(KC_E), MEH(KC_R), MEH(KC_T), MEH(KC_Y), MEH(KC_U), MEH(KC_I), MEH(KC_O), MEH(KC_P),  KC_BSPC,
+  KC_NO,   KC_NO,     KC_PSCR,   KC_HOME,   KC_PGUP,   KC_NO,     KC_PSLS,   KC_P7,     KC_P8,     KC_P9,     KC_PMNS,    KC_NO,
+  KC_DEL,  KC_NO,     KC_NO,     KC_END,    KC_PGDN,   KC_NO,     KC_PAST,   KC_P4,     KC_P5,     KC_P6,     KC_PPLS,    KC_NO,
+  KC_NO,   KC_WH_L,   KC_WH_D,   KC_WH_U,   KC_WH_R,   KC_NO,     KC_P0,     KC_P1,     KC_P2,     KC_P3,     KC_EQL,     KC_NO,
+  KC_NO,   KC_NO,     KC_NO,     LOWER,     KC_NO,     KC_UNDS,   KC_SPC,    KC_P0,     RAISE,     KC_PDOT,   LGUI(KC_L), LCA(KC_DEL)
 ),
 
 [_RAISE] = LAYOUT_preonic_grid (
-  KC_GRV,  KC_NO,   KC_NO,   KC_LT,   KC_GT,   KC_NO,   KC_NO,  KC_LT,   KC_GT,   KC_NO,   KC_NO,   KC_DEL,
-  KC_NO,   KC_NO,   KC_NO,   KC_LPRN, KC_RPRN, KC_NO,   KC_NO,  KC_LPRN, KC_RPRN, KC_NO,   KC_NO,   KC_NO,
-  KC_DEL,  KC_PMNS, KC_PLUS, KC_LBRC, KC_RBRC, KC_NO,   KC_NO,  KC_LBRC, KC_RBRC, KC_PMNS, KC_PLUS, KC_NO,
-  KC_NO,   KC_UNDS, KC_EQL,  KC_LCBR, KC_RCBR, KC_NO,   KC_NO,  KC_LCBR, KC_RCBR, KC_UNDS, KC_EQL,  KC_NO,
-  KC_NO,   KC_NO,   KC_NO,   KC_NO,   LOWER,   KC_UNDS, KC_SPC, RAISE,   KC_NO,   KC_NO,   KC_NO,   LCA(KC_DEL)
+  KC_GRV,  KC_NO,   KC_NO,   KC_LT,   KC_GT,   KC_NO,   KC_NO,  KC_LT,   KC_GT,   KC_NO,   KC_NO,      KC_DEL,
+  KC_NO,   KC_NO,   KC_NO,   KC_LPRN, KC_RPRN, KC_NO,   KC_NO,  KC_LPRN, KC_RPRN, KC_NO,   KC_NO,      KC_NO,
+  KC_DEL,  KC_PMNS, KC_PLUS, KC_LBRC, KC_RBRC, KC_NO,   KC_NO,  KC_LBRC, KC_RBRC, KC_PMNS, KC_PLUS,    KC_NO,
+  KC_NO,   KC_UNDS, KC_EQL,  KC_LCBR, KC_RCBR, KC_NO,   KC_NO,  KC_LCBR, KC_RCBR, KC_UNDS, KC_EQL,     KC_NO,
+  KC_NO,   KC_NO,   KC_NO,   LOWER,   KC_NO,   KC_UNDS, KC_SPC, KC_NO,   RAISE,   KC_NO,   LGUI(KC_L), LCA(KC_DEL)
 ),
 
 [_ADJUST] = LAYOUT_preonic_grid(
