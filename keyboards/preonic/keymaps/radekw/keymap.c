e5861f231d0856c6443f3c1cbfef46f3aeac9358
Radek Wierzbicki <radekw@me.com>
2022-12-18 20:28:27 -0500

    another attempt at making things easier

--- keyboards/preonic/keymaps/radekw/keymap.c
+++ keyboards/preonic/keymaps/radekw/keymap.c
@@ -39,15 +39,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,        KC_W,        KC_E,  KC_R,    KC_T,       KC_Y,       KC_U,   KC_I,    KC_O,          KC_P,           KC_BSLS,
   KC_ESC,  KC_A,        KC_S,        KC_D,  KC_F,    LT(1,KC_G), LT(2,KC_H), KC_J,   KC_K,    KC_L,          KC_SCLN,        KC_QUOT,
   KC_LSFT, CTL_T(KC_Z), ALT_T(KC_X), KC_C,  KC_V,    LT(4,KC_B), KC_N,       KC_M,   KC_COMM, ALT_T(KC_DOT), CTL_T(KC_SLSH), KC_RSFT,
-  MO(4),   KC_LGUI,     KC_NO,       LOWER, KC_BSPC, KC_ENT,     KC_SPC,     KC_SPC, RAISE,   KC_NO,         KC_NO,          KC_NO
+  KC_NO,   KC_NO,       MO(4),       LOWER, KC_BSPC, KC_ENT,     KC_SPC,     KC_SPC, RAISE,   KC_LGUI,       KC_NO,          KC_NO
 ),
 
 [_LOWER] = LAYOUT_preonic_grid(
-  KC_TILD, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,  KC_NO,  KC_LPRN,  KC_RPRN,    KC_BSPC,
-  KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_PSLS, KC_P7,  KC_P8,  KC_P9,    KC_PMNS,    KC_NO,
-  KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_PAST, KC_P4,  KC_P5,  KC_P6,    KC_PPLS,    KC_NO,
-  KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_P0,   KC_P1,  KC_P2,  KC_P3,    KC_EQL,     KC_NO,
-  KC_NO,   KC_NO,  KC_NO,  LOWER,  KC_DEL, KC_ENT, KC_SPC,  KC_P0,  RAISE,  KC_PDOT,  KC_NO,      KC_NO
+  KC_TILD, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,  KC_NO,  KC_LPRN,  KC_RPRN,  KC_BSPC,
+  KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_PSLS, KC_P7,  KC_P8,  KC_P9,    KC_PMNS,  KC_NO,
+  KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_PAST, KC_P4,  KC_P5,  KC_P6,    KC_PPLS,  KC_NO,
+  KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_P0,   KC_P1,  KC_P2,  KC_P3,    KC_EQL,   KC_NO,
+  KC_NO,   KC_NO,  KC_NO,  LOWER,  KC_DEL, KC_ENT, KC_SPC,  KC_P0,  RAISE,  KC_PDOT,  KC_NO,    KC_NO
 ),
 
 [_RAISE] = LAYOUT_preonic_grid (
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_NO,   MEH(KC_Q), MEH(KC_W), MEH(KC_E), MEH(KC_R), KC_NO,   KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_NO,   KC_PSCR,
   KC_NO,   KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,   LCA(KC_DEL),
   KC_NO,   KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_TRNS, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_UP,   KC_NO,
-  KC_TRNS, KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT
+  KC_NO,   KC_NO,     KC_TRNS,   KC_NO,     KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT
 )
 };
 
