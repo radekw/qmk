aa8875acce189400100a7788bcb36cedd9ef8a6b
Radek Wierzbicki <radekw@me.com>
2022-12-21 22:56:14 -0500

    preonic improvements

--- keyboards/preonic/keymaps/radekw/keymap.c
+++ keyboards/preonic/keymaps/radekw/keymap.c
@@ -35,11 +35,11 @@ enum preonic_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_preonic_grid(
-  QK_GESC, KC_1,        KC_2,    KC_3,  KC_4,    KC_5,       KC_6,       KC_7,   KC_8,    KC_9,          KC_0,           KC_MINS,
-  KC_TAB,  KC_Q,        KC_W,    KC_E,  KC_R,    KC_T,       KC_Y,       KC_U,   KC_I,    KC_O,          KC_P,           KC_BSLS,
-  KC_ESC,  KC_A,        KC_S,    KC_D,  KC_F,    LT(1,KC_G), LT(2,KC_H), KC_J,   KC_K,    KC_L,          KC_SCLN,        KC_QUOT,
-  KC_LSFT, CTL_T(KC_Z), KC_X,    KC_C,  KC_V,    LT(4,KC_B), KC_N,       KC_M,   KC_COMM, ALT_T(KC_DOT), CTL_T(KC_SLSH), KC_RSFT,
-  KC_LCTL, KC_NO,       KC_LALT, LOWER, KC_BSPC, KC_ENT,     KC_SPC,     KC_SPC, RAISE,   KC_LGUI,       KC_NO,           KC_NO
+  QK_GESC, KC_1,        KC_2,    KC_3,  KC_4,       KC_5,   KC_6,   KC_7,       KC_8,    KC_9,    KC_0,           KC_MINS,
+  KC_TAB,  KC_Q,        KC_W,    KC_E,  KC_R,       KC_T,   KC_Y,   KC_U,       KC_I,    KC_O,    KC_P,           KC_BSLS,
+  KC_BSPC, KC_A,        KC_S,    KC_D,  LT(1,KC_F), KC_G,   KC_H,   LT(2,KC_J), KC_K,    KC_L,    KC_SCLN,        KC_QUOT,
+  KC_LSFT, CTL_T(KC_Z), KC_X,    KC_C,  LT(4,KC_V), KC_B,   KC_N,   KC_M,       KC_COMM, KC_DOT,  CTL_T(KC_SLSH), SC_SENT,
+  KC_LCTL, KC_NO,       KC_LALT, LOWER, KC_BSPC,    KC_ENT, KC_SPC, KC_SPC,     RAISE,   KC_LGUI, KC_NO,          KC_NO
 ),
 
 [_LOWER] = LAYOUT_preonic_grid(
@@ -59,11 +59,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_ADJUST] = LAYOUT_preonic_grid(
-  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,     KC_NO,
-  BACKLIT,  QK_BOOT,  DB_TOGG,  RGB_TOG,  RGB_RMOD, RGB_MOD,  RGB_SPD,  RGB_SPI,  KC_NO,    KC_NO,    KC_NO,     KC_NO,
-  KC_NO,    KC_NO,    MU_NEXT,  AU_ON,    AU_OFF,   AG_NORM,  AG_SWAP,  KC_NO,    KC_NO,    KC_PSCR,  KC_HOME,   KC_PGUP,
-  KC_NO,    AU_PREV,  AU_NEXT,  MU_ON,    MU_OFF,   MI_ON,    MI_OFF,   KC_NO,    KC_NO,    KC_NO,    KC_END,    KC_PGDN,
-  KC_VOLD,  KC_VOLU,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_MPRV,  KC_MPLY,   KC_MNXT
+  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+  BACKLIT,  QK_BOOT,  DB_TOGG,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+  KC_NO,    KC_NO,    MU_NEXT,  AU_ON,    AU_OFF,   AG_NORM,  AG_SWAP,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+  KC_NO,    AU_PREV,  AU_NEXT,  MU_ON,    MU_OFF,   MI_ON,    MI_OFF,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+  KC_VOLD,  KC_VOLU,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_MPRV,  KC_MPLY,  KC_MNXT
 ),
 
 [_NAV] = LAYOUT_preonic_grid(
