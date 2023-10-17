80b1fde1a3b435570567d6eab3f0a8c4936a7ae1
Radek Wierzbicki <radekw@me.com>
2023-10-16 20:38:51 -0400

    adding game layer

--- keyboards/charybdis/4x6/keymaps/radekw/keymap.c
+++ keyboards/charybdis/4x6/keymaps/radekw/keymap.c
@@ -9,6 +9,7 @@
 enum charybdis_keymap_layers {
     LAYER_QWERTY = 0,
     LAYER_COLEMAKDH,
+    LAYER_GAMING,
     LAYER_NUM,
     LAYER_NAV,
     LAYER_SYM,
@@ -18,7 +19,8 @@ enum charybdis_keymap_layers {
 
 enum custom_keycodes {
     KC_QWER = SAFE_RANGE,
-    KC_COLE
+    KC_COLE,
+    KC_GAME
 };
 
 /* Automatically enable sniping-mode on the pointer layer. */
@@ -88,6 +90,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
+  [LAYER_GAMING] = LAYOUT(
+  // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
+        KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_DEL,
+  // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
+          KC_T,  KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS,
+  // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
+          KC_G, KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,       KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+  // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
+          KC_B, KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,       KC_N,    KC_M, KC_COMM,  KC_DOT,  C_SLSH, SC_SENT,
+  // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
+                                    L_NAV,  KC_SPC,    L_FN,      L_NUM,  KC_SPC,
+                                           KC_LALT,  KC_ENT,      L_SYM
+  //                            ╰───────────────────────────╯ ╰──────────────────╯
+  ),
+
   [LAYER_NUM] = LAYOUT(
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
         KC_ESC,  KC_NUM,   KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
@@ -135,7 +152,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [LAYER_FN] = LAYOUT(
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
-         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    CG_TOGG, KC_QWER, KC_COLE,   KC_NO, DB_TOGG, QK_BOOT,
+         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    CG_TOGG, KC_QWER, KC_COLE, KC_GAME, DB_TOGG, QK_BOOT,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
          KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_F7,   KC_F8,   KC_F9,  KC_F12, KC_PSCR,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
@@ -205,6 +222,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case KC_COLE:
             if (record->event.pressed) {
                 set_single_persistent_default_layer(LAYER_COLEMAKDH);
+            }
+            return false;
+            break;
+        case KC_GAME:
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(LAYER_GAMING);
             }
             return false;
             break;
--- keyboards/preonic/keymaps/radekw/keymap.c
+++ keyboards/preonic/keymaps/radekw/keymap.c
@@ -16,6 +16,7 @@
 enum preonic_layers {
   _QWERTY,
   _COLEMAK,
+  _GAME,
   _NUM,
   _NAV,
   _SYM,
@@ -25,6 +26,7 @@ enum preonic_layers {
 enum preonic_keycodes {
   KC_QWER = SAFE_RANGE,
   KC_COLE,
+  KC_GAME,
   BACKLIT,
   VIM_WA,
   VIM_QA
@@ -45,8 +47,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LSFT, C_Z,   KC_X,    KC_C, KC_D,  KC_V,   KC_K,   KC_H,  KC_COMM, KC_DOT,  C_SLSH,  SC_SENT,
   KC_LCTL, KC_NO, KC_LALT, L_FN, L_NAV, KC_ENT, KC_SPC, L_NUM, L_SYM,   KC_LGUI, KC_NO,   KC_RCTL
 ),
+[_GAME] = LAYOUT_preonic_grid(
+  KC_ESC,  KC_1,  KC_2,    KC_3, KC_4,  KC_5,   KC_6,   KC_7,  KC_8,    KC_9,    KC_0,    KC_DEL,
+  KC_TAB,  KC_Q,  KC_W,    KC_E, KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSLS,
+  KC_BSPC, KC_A,  KC_S,    KC_D, KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LSFT, KC_Z,  KC_X,    KC_C, KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, SC_SENT,
+  KC_LCTL, KC_NO, KC_LALT, L_FN, L_NAV, KC_SPC, KC_SPC, L_NUM, L_SYM,   KC_LGUI, KC_NO,   KC_RCTL
+),
 [_NUM] = LAYOUT_preonic_grid(
-  KC_ESC,  KC_NUM,  KC_NO, KC_NO,  KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO,
+  KC_GRV,  KC_NUM,  KC_NO, KC_NO,  KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO,
   KC_NO,   KC_PSLS, KC_P7, KC_P8,  KC_P9,  KC_PMNS, KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO,
   KC_BSPC, KC_PAST, KC_P4, KC_P5,  KC_P6,  KC_PPLS, KC_NO,   KC_RSFT, KC_NO,   KC_NO, KC_NO, KC_NO,
   KC_DEL,  KC_P0,   KC_P1, KC_P2,  KC_P3,  KC_EQL,  KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO,
@@ -67,11 +76,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_NO,   KC_NO,   KC_NO,   KC_DOT,  KC_DOT,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO
 ),
 [_FN] = LAYOUT_preonic_grid(
-  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   CG_TOGG, KC_QWER, KC_COLE, KC_NO, DB_TOGG, QK_BOOT,
-  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_F7,   KC_F8,   KC_F9, KC_F12,  KC_PSCR,
-  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_F4,   KC_F5,   KC_F6, KC_F11,  A_C_D,
-  KC_MUTE, KC_VOLD, KC_VOLU, KC_MPRV, KC_MNXT, KC_MPLY, KC_NO,   KC_F1,   KC_F2,   KC_F3, KC_F10,  KC_NO,
-  KC_NO,   KC_NO,   KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO,   KC_NO
+  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   CG_TOGG, KC_QWER, KC_COLE, KC_GAME, DB_TOGG, QK_BOOT,
+  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_F7,   KC_F8,   KC_F9,   KC_F12,  KC_PSCR,
+  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_F4,   KC_F5,   KC_F6,   KC_F11,  A_C_D,
+  KC_MUTE, KC_VOLD, KC_VOLU, KC_MPRV, KC_MNXT, KC_MPLY, KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F10,  KC_NO,
+  KC_NO,   KC_NO,   KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,   KC_NO,   KC_NO
 )
 };
 
@@ -86,6 +95,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case KC_COLE:
       if (record->event.pressed) {
         set_single_persistent_default_layer(_COLEMAK);
+      }
+      return false;
+      break;
+    case KC_GAME:
+      if (record->event.pressed) {
+        set_single_persistent_default_layer(_GAME);
       }
       return false;
       break;
