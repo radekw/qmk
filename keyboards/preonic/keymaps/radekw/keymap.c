9b769e54a151406d4428a373534cfb8c9738a2c9
Radek Wierzbicki <radekw@me.com>
2024-01-17 21:50:36 -0500

    preonic update

--- keyboards/preonic/keymaps/radekw/keymap.c
+++ keyboards/preonic/keymaps/radekw/keymap.c
@@ -34,11 +34,11 @@ enum preonic_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_preonic_grid(
-  KC_GRV,  KC_1,  KC_2,    KC_3, KC_4,  KC_5,   KC_6,   KC_7,  KC_8,    KC_9,    KC_0,    KC_DEL,
-  KC_TAB,  KC_Q,  KC_W,    KC_E, KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSLS,
-  KC_BSPC, KC_A,  KC_S,    KC_D, KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT, C_Z,   KC_X,    KC_C, KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  C_SLSH,  SC_SENT,
-  KC_LCTL, KC_NO, KC_LALT, L_FN, L_NAV, KC_ENT, KC_SPC, L_NUM, L_SYM,   KC_LGUI, KC_NO,   KC_RCTL
+  KC_GRV,  KC_1,    KC_2,    KC_3, KC_4,  KC_5,   KC_6,   KC_7,  KC_8,    KC_9,    KC_0,    KC_DEL,
+  KC_TAB,  KC_Q,    KC_W,    KC_E, KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSLS,
+  KC_BSPC, KC_A,    KC_S,    KC_D, KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LSFT, C_Z,     KC_X,    KC_C, KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  C_SLSH,  SC_SENT,
+  KC_LCTL, KC_LCMD, KC_LALT, L_FN, L_NAV, KC_ENT, KC_SPC, L_NUM, L_SYM,   KC_LGUI, KC_RCMD, KC_RCTL
 ),
 [_COLEMAK] = LAYOUT_preonic_grid(
   KC_GRV,  KC_1,  KC_2,    KC_3, KC_4,  KC_5,   KC_6,   KC_7,  KC_8,    KC_9,    KC_0,    KC_DEL,
